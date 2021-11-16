41caea07dfa4d430ddd6e0a1e9b54709bf1022dd
Will Williamson <coder3223@gmail.com>
2021-11-15 22:48:56 -0500

    Basic model set-up

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 //  imports
-const { urlencoded } = require('express');
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const PORT = 5500;
-// Create create app 
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
+const Campground = require('./models/campground');
+
+// connection to mongoose
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+// Create create app
 const app = express();
 
 // Setting up EJS
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Creating route for homepage
-app.get('/', (req, res) => {
-    res.render('home');
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+// Creating route for homepage
+app.get("/makecampground", async (req, res) => {
+    const camp = new Campground({title: 'My Backyard', description: 'free camping'});
+    await camp.save();
+    res.send(camp);
+  });
 
 // Set server port
 app.listen(PORT, () => {
-    console.log(`Serving on port: ${PORT}`)
-})
+  console.log(`Serving on port: ${PORT}`);
+});
---
+++ models/campground.js
@@ -0,0 +1,15 @@
+// Imports
+const mongoose = require('mongoose');
+// Create shorter reference for schema so I don't have to type mongoose everytime
+const Schema = mongoose.Schema;
+
+// Creating the campground schema
+const CampgroundSchema = new Schema({
+    title: String,
+    price: String,
+    description: String,
+    location: String
+});
+
+// Export the model to connect to mongoose
+module.exports = mongoose.model('Campground', CampgroundSchema);
