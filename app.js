1cb3344e56e910542413fc1f57e989cd0c3a42d3
Will Williamson <coder3223@gmail.com>
2021-11-18 15:13:32 -0500

    add async error wrapper

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const morgan = require('morgan');
 const ejsMate = require('ejs-mate');
-
+const ExpressError = require('./utils/ExpressError');
+const catchAsync = require('./utils/catchAsync');
 // connection to mongoose
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
@@ -36,48 +37,52 @@ app.get("/", (req, res) => {
 });
 
 // Creating different routes for different campgrounds
-app.get("/campgrounds", async (req, res) => {
+app.get("/campgrounds", catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
-  });
+  }));
 
 // Create route to server from for editing a campground
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new'); 
-})
+});
 
 // Create route to recieve data from the form
-app.post('/campgrounds/', async (req, res) => {
+app.post('/campgrounds', catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}));
 
 //  Create route for a details campground page
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
-})
+}));
 
 // Create route for editing campgrounds
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/edit', { campground });
-})
+}));
 
 // Create a route to update campground
-app.put('/campgrounds/:id', async(req, res) => {
+app.put('/campgrounds/:id', catchAsync(async(req, res) => {
   const { id } = req.params;
   const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
   res.redirect(`/campgrounds/${campground._id}`);
-})
+}));
 
 // Create a route to delete campground
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
   await Campground.findByIdAndDelete(id);
   res.redirect('/campgrounds');
-})
+}));
+
+app.use((err, req, res, next) => {
+  res.send("Something went wrong!!!");
+});
 
 // Set server port
 app.listen(PORT, () => {
