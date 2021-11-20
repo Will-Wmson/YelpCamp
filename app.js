c6ea1f32f25ca98a362a1279a0e8ef8bfc88cf36
Will Williamson <coder3223@gmail.com>
2021-11-20 09:51:49 -0500

    add reviews and validate on client side and server side

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const morgan = require('morgan');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError');
 const catchAsync = require('./utils/catchAsync');
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
+const Review = require('./models/reviews')
+
 // connection to mongoose
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
@@ -31,6 +34,28 @@ app.use(express.urlencoded({extended: true}));
 // Setup method-Override to use PUT
 app.use(methodOverride('_method'));
 
+// Validating Javascript Object(usig Joi) for reviews before passing to Mongoose
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map(el => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+}
+
+// Validating Javascript Object(usig Joi) for campgrounds before passing to Mongoose
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if(error) {
+    const msg = error.details.map(el => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+}
+
 // Creating route for homepage
 app.get("/", (req, res) => {
   res.render("home");
@@ -47,9 +72,9 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new'); 
 });
 
-// Create route to recieve data from the form
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
-  if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+// Create route to recieve data from the form using Joi to validate entries before sending to mongoose
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+  // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
@@ -68,7 +93,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
 }));
 
 // Create a route to update campground
-app.put('/campgrounds/:id', catchAsync(async(req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async(req, res) => {
   const { id } = req.params;
   const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
   res.redirect(`/campgrounds/${campground._id}`);
@@ -81,10 +106,22 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
   res.redirect('/campgrounds');
 }));
 
+// Create a route for campground reviews
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  const review = new Review(req.body.review);
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+// Middleware error message
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page Not Found', 404));
 })
 
+// Middleware error message
 app.use((err, req, res, next) => {
   const { statusCode = 500 } = err;
   if(!err.message) err.message = 'Something Went Wrong!';
