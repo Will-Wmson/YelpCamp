ea354e6bf546a8f627c2ef9a011766db68efca5f
Will Williamson <coder3223@gmail.com>
2021-12-15 20:59:20 -0500

    start authentication

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError");
-const campgrounds = require("./routes/campgrounds");
-const reviews = require("./routes/reviews");
+const userRoutes = require("./routes/users");
+const campgroundRoutes = require("./routes/campgrounds");
+const reviewRoutes = require("./routes/reviews");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
 // connection to mongoose
@@ -27,11 +31,6 @@ app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Setup Parser
-app.use(express.urlencoded({ extended: true }));
-// Setup method-Override to use PUT
-app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, "public")));
 // setup sessions
 const sessionConfig = {
   secret: "thisshouldbeabettersecret!",
@@ -43,8 +42,20 @@ const sessionConfig = {
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
+
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Setup Parser
+app.use(express.urlencoded({ extended: true }));
+// Setup method-Override to use PUT
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Middleware to handle flash messages
 app.use((req, res, next) => {
@@ -53,8 +64,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
 // Creating route for homepage
 app.get("/", (req, res) => {
